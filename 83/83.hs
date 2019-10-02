3ca027f9fa9342d5cac700df2a58f21d659a278f
Anuj <anuj.middha@gmail.com>
2019-10-02 12:14:46 +0530

    solved 83

--- 83/83.hs
+++ 83/83.hs
@@ -5,12 +5,13 @@ import System.Environment
 import Data.List.Split
 
 main = do
-    s <- readFile "p081_matrix.txt"
-    print (calculateAnswer s)
+    s <- readFile "p083_matrix.txt"
+    print (head (head (calculateAnswer s)))
 
-calculateAnswer :: [Char] -> Int
-calculateAnswer input = head (head (shortestPaths matrix lastX lastY emptyPaths))
+calculateAnswer :: [Char] -> [[Int]]
+calculateAnswer input = shortestPaths matrix lastX lastY firstPass
     where
+        firstPass = shortestPaths matrix lastX lastY emptyPaths
         matrix = parseMatrix input
         parseMatrix input = map parseLine (lines input)
         parseLine line = map parseInt (splitOn "," line)
@@ -25,11 +26,27 @@ shortestPaths matrix x y preCalculatedPath
     | x == 0 && y == 0 = updatedPaths
     | otherwise        = shortestPaths matrix (fst nextPosition) (snd nextPosition) updatedPaths
     where
-        updatedPaths = updatePosition matrix x y preCalculatedPath
+        updatedPaths = updateNeighbours matrix ((x,y) : (neighbours matrix x y)) preCalculatedPath
         nextPosition 
             | x == 0                   = (y-1, 0)
             | y == (length matrix - 1) = (length (head matrix) - 1, x-1)
             | otherwise                = (x-1, y+1)
+
+neighbours :: [[Int]] -> Int -> Int -> [(Int, Int)]
+neighbours matrix x y = filter isValid [(x-1,y), (x,y-1), (x+1,y), (x,y+1)]
+    where
+        isValid point = (pointX >= 0 && pointX <= lastX) && (pointY >= 0 && pointY <= lastY)
+            where
+                pointX = fst point
+                pointY = snd point
+        lastX = length (head matrix) - 1
+        lastY = length matrix - 1
+
+updateNeighbours :: [[Int]] -> [(Int, Int)] -> [[Int]] -> [[Int]]
+updateNeighbours matrix [] preCalculatedPath = preCalculatedPath
+updateNeighbours matrix (point:points) preCalculatedPath = updateNeighbours matrix points updatedPaths
+    where
+        updatedPaths = updatePosition matrix (fst point) (snd point) preCalculatedPath
 
 updatePosition :: [[Int]] -> Int -> Int -> [[Int]] -> [[Int]]
 updatePosition matrix x y preCalculatedPath = preRows ++ [(preColumns ++ [shortestPath] ++ postCoumns)] ++ postRows
@@ -42,8 +59,6 @@ updatePosition matrix x y preCalculatedPath = preRows ++ [(preColumns ++ [shorte
         pathAt x' y' = (preCalculatedPath !! y') !! x'
         shortestPath
             | y == (length matrix - 1) && x == (length (head matrix) - 1) = element
-            | y == (length matrix - 1)                                = element + pathAt (x+1) y
-            | x == (length (head matrix) - 1)                         = element + pathAt x (y+1)
-            | otherwise                                               = element + min (pathAt x (y+1)) (pathAt (x+1) y)
+            | otherwise                                                   = element + foldr1 min (filter (>0) (map (\p -> pathAt (fst p) (snd p)) (neighbours matrix x y)))
             where
                 element = (matrix !! y) !! x
